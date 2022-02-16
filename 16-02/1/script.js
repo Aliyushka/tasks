30a3ad36a100cc3c65b71b5f6b12a92ef7a6cb54
Aliya <kozhobekova0424@gmail.com>
2022-02-16 11:25:04 +0600

    feat: finished HW 16-02

---
+++ 16-02/1/script.js
@@ -0,0 +1,19 @@
+/*1*/
+let kozhobekovaAliya = 1;
+alert(kozhobekovaAliya);
+
+kozhobekova_aliya = 2;
+alert(kozhobekova_aliya);
+
+KozhobekovaAliya = 3;
+alert(KozhobekovaAliya);
+console.log(kozhobekovaAliya, kozhobekova_aliya, KozhobekovaAliya);
+
+
+
+/*2*/
+let str1 = 'САЛАМ, ';
+let str2 = 'БИШКЕК!';
+alert(str1 + str2);
+
+console.log(str1 + str2);
