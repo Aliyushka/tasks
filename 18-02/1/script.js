e776b47f2c1b473ef9964a850fdc9b935e65e21a
Aliya <kozhobekova0424@gmail.com>
2022-02-18 19:49:20 +0600

    init: finished homework 18-02

---
+++ 18-02/1/script.js
@@ -0,0 +1,56 @@
+// ---- homewwork #1 --- //
+/*
+let num = 10;
+num++;
+num++;
+num--;
+console.log(num);
+*/
+
+
+// ----homewwork #2  --- //
+let name = 'Aliya';
+alert('Привет ' + name);
+
+
+// ----homewwork #3  --- //
+let age = 28;
+alert('Мне ' + age + ' лет');
+
+
+// ----homewwork #4 --- //
+let a = 17;
+let b = 10;
+let c = a - 10;
+let d = 7;
+let result = c * d;
+console.log(result);
+
+
+
+// // ----homewwork #5  --- //
+
+let num = 47;
+num += 7;
+num -= 18;
+num *= 10;
+num /= 15;
+alert(num)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
